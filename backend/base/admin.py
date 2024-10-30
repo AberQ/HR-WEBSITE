8946d4653ddcfdf697ff1795426251745d54efe0
AberQ <egor.master2017@gmail.com>
2024-10-30 15:21:32 +0500

    Перенос admin.py в директорию проекта

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Vacancy, TechStackTag
+from api.models import Vacancy, TechStackTag
 from django.core.exceptions import ValidationError
 
 @admin.register(Vacancy)
