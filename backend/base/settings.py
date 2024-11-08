8d837e3cad29daa9380c579a2c091b74e6c5e9fe
AberQ <egor.master2017@gmail.com>
2024-11-08 23:03:07 +0500

    Смена базы данных на PostgreSQL

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'JobSite',  # Название базы данных
+        'USER': 'postgres',  # Пользователь базы данных
+        'PASSWORD': '123',  # Пароль для пользователя базы данных
+        'HOST': 'localhost',  # Адрес хоста (обычно localhost)
+        'PORT': '5432',  # Порт PostgreSQL (по умолчанию 5432)
     }
 }
 
