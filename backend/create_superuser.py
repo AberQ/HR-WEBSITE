1588a28025e5fcd716d636450e6a3f2a96ed014f
AberQ <egor.master2017@gmail.com>
2024-11-10 20:31:15 +0500

    Добавление проверок на наличие пользователей с такой почтой при развертывании Docker

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -19,28 +19,41 @@ try:
         print("Superuser created!")
     else:
         print("Superuser already exists.")
+    
+    # Проверяем, существует ли уже работодатель с таким email
+    if not Employer.objects.filter(email='employer12345@example.com').exists():
+        # Создаем работодателя (Employer)
+        employer_email = 'employer123456@example.com'
+        employer_password = 'Buch555789'
+        employer = Employer.objects.create(
+            email=employer_email,
+            password=employer_password,
+            company_name='Example Company',
+            company_info='Some information about the company'
+        )
+        employer.save()
+        print("Employer created!")
+    else:
+        print("Employer with this email already exists.")
+    
+    # Проверяем, существует ли уже соискатель с таким email
+    if not Applicant.objects.filter(email='applicant12345@example.com').exists():
+        # Создаем соискателя (Applicant)
+        applicant_email = 'applicant123456@example.com'
+        applicant_password = 'Buch555789'
+        applicant = Applicant.objects.create(
+            email=applicant_email,
+            password=applicant_password,
+            first_name='John',
+            last_name='Doe',
+            patronymic='Ivanovich'
+        )
+        applicant.save()
+        print("Applicant created!")
+    else:
+        print("Applicant with this email already exists.")
+    
 except ImproperlyConfigured:
     print("Не удалось найти модель пользователя. Проверьте AUTH_USER_MODEL в settings.py")
-employer_email = 'employer12345@example.com'
-employer_password = 'Buch555789'  # Конкретный пароль для работодателя
-employer = Employer.objects.create(
-    email=employer_email,
-    password=employer_password,  # Устанавливаем конкретный пароль
-    company_name='Example Company',
-    company_info='Some information about the company'
-)
-employer.save()
-
-# Создаем пользователя Applicant
-applicant_email = 'applicant12345@example.com'
-applicant_password = 'Buch555789'  # Конкретный пароль для соискателя
-applicant = Applicant.objects.create(
-    email=applicant_email,
-    password=applicant_password,  # Устанавливаем конкретный пароль
-    first_name='John',
-    last_name='Doe',
-    patronymic='Ivanovich'
-)
-applicant.save()
-
-print("Employer and Applicant have been added to the database.")
+except Exception as e:
+    print(f"Произошла ошибка: {e}")
