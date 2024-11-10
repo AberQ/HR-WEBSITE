e07078065c8b243c131764103448b7c9622de6cf
AberQ <egor.master2017@gmail.com>
2024-11-10 20:55:34 +0500

    Добавлено хэширование для паролей

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -1,4 +1,3 @@
-#Этот файл нужен для автоматического создания пользователей, вакансий и резюме для тестирования
 import os
 import django
 from django.core.exceptions import ImproperlyConfigured
@@ -7,41 +6,36 @@ from django.contrib.auth import get_user_model
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 django.setup()
 from registration.models import Employer, Applicant
+
 # Получаем модель пользователя
 User = get_user_model()
 
-
-#Входные данные
+# Входные данные
 admin_email = 'admin@example.com'
-admin_password = 'Test555789'
+admin_password = '1'
 
 employer_email = 'employer@example.com'
-employer_password = 'Test555789'
+employer_password = '1'
 
 applicant_email = 'applicant@example.com'
-applicant_password = 'Test555789'
+applicant_password = '1'
 
 try:
     # Проверяем, существует ли суперпользователь с заданным email
     if not User.objects.filter(email=admin_email).exists():
-        User.objects.create_superuser(
-            email=admin_email,
-            password=admin_password
-        )
+        admin_user = User(email=admin_email)
+        admin_user.set_password(admin_password)  # Хешируем пароль
+        admin_user.is_superuser = True
+        admin_user.is_staff = True
+        admin_user.save()
         print(f"Superuser {admin_email} created!")
     else:
         print(f"Superuser {admin_email} already exists.")
     
     # Проверяем, существует ли уже работодатель с таким email
     if not Employer.objects.filter(email=employer_email).exists():
-        # Создаем работодателя (Employer)
-        
-        employer = Employer.objects.create(
-            email=employer_email,
-            password=employer_password,
-            company_name='Example Company',
-            company_info='Some information about the company'
-        )
+        employer = Employer(email=employer_email, company_name='Example Company', company_info='Some information about the company')
+        employer.set_password(employer_password)  # Хешируем пароль
         employer.save()
         print(f"Employer {employer_email} created!")
     else:
@@ -49,19 +43,12 @@ try:
     
     # Проверяем, существует ли уже соискатель с таким email
     if not Applicant.objects.filter(email=applicant_email).exists():
-        # Создаем соискателя (Applicant)
-        
-        applicant = Applicant.objects.create(
-            email=applicant_email,
-            password=applicant_password,
-            first_name='John',
-            last_name='Doe',
-            patronymic='Ivanovich'
-        )
+        applicant = Applicant(email=applicant_email, first_name='John', last_name='Doe', patronymic='Ivanovich')
+        applicant.set_password(applicant_password)  # Хешируем пароль
         applicant.save()
         print(f"Applicant {applicant_email} created!")
     else:
-        print(f"fApplicant with {applicant_email} already exists.")
+        print(f"Applicant with {applicant_email} already exists.")
     
 except ImproperlyConfigured:
     print("Не удалось найти модель пользователя. Проверьте AUTH_USER_MODEL в settings.py")
