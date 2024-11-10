26922c50901731c931fabc69c6eed44bf6905d45
AberQ <egor.master2017@gmail.com>
2024-11-10 20:23:13 +0500

    Теперь автоматически создаются Employer и Applicant при развертке docker

--- backend/create_superuser.py
+++ backend/create_superuser.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
 django.setup()
-
+from registration.models import Employer, Applicant
 # Получаем модель пользователя
 User = get_user_model()
 
@@ -21,3 +21,26 @@ try:
         print("Superuser already exists.")
 except ImproperlyConfigured:
     print("Не удалось найти модель пользователя. Проверьте AUTH_USER_MODEL в settings.py")
+employer_email = 'employer12345@example.com'
+employer_password = 'Buch555789'  # Конкретный пароль для работодателя
+employer = Employer.objects.create(
+    email=employer_email,
+    password=employer_password,  # Устанавливаем конкретный пароль
+    company_name='Example Company',
+    company_info='Some information about the company'
+)
+employer.save()
+
+# Создаем пользователя Applicant
+applicant_email = 'applicant12345@example.com'
+applicant_password = 'Buch555789'  # Конкретный пароль для соискателя
+applicant = Applicant.objects.create(
+    email=applicant_email,
+    password=applicant_password,  # Устанавливаем конкретный пароль
+    first_name='John',
+    last_name='Doe',
+    patronymic='Ivanovich'
+)
+applicant.save()
+
+print("Employer and Applicant have been added to the database.")
