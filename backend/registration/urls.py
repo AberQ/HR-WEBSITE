d8b4ac772c82ae1eb03e8d3cf616ed9debca4c4d
AberQ <egor.master2017@gmail.com>
2024-11-24 23:41:57 +0500

    Привел URLS в состояние до форматирования(их удобнее читать так)

--- backend/registration/urls.py
+++ backend/registration/urls.py
@@ -5,28 +5,23 @@ from api.views.views_api_vacancies import *
 from registration.views import *
 
 urlpatterns = [
-    path("choice_registration/", choice_registration, name="choice_registration"),
-    path(
-        "applicant_registration/", applicant_registration, name="applicant_registration"
-    ),
-    path("employer_registration/", employer_registration, name="employer_registration"),
-    path("login/", custom_login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
-    path(
-        "api/register/applicant",
-        RegisterApplicantView.as_view(),
-        name="register_applicant",
-    ),
-    path(
-        "api/register/employer",
-        RegisterEmployerView.as_view(),
-        name="register_employer",
-    ),
-    path("api/login/profile/", UserProfileView.as_view(), name="user-profile"),
-    path(
-        "api/login/token/",
-        CustomTokenObtainPairView.as_view(),
-        name="token_obtain_pair",
-    ),
-    path("api/login/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path('choice_registration/', choice_registration, name='choice_registration'),
+    path('applicant_registration/', applicant_registration, name='applicant_registration'),
+    path('employer_registration/', employer_registration, name='employer_registration'),
+    
+
+    path('login/', custom_login_view, name='login'),
+    path('logout/', logout_view, name='logout'),  
+    
+
+
+    
+    path('api/register/applicant', RegisterApplicantView.as_view(), name='register_applicant'),
+    path('api/register/employer', RegisterEmployerView.as_view(), name='register_employer'),
+
+
+    path('api/login/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('api/login/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
